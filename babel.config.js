af07d083af2ef088c158352a271ae6811bea8765
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-16 22:34:13 -0400

    Add tamagui

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,16 @@ module.exports = (api) => {
 
   return {
     presets: [['babel-preset-expo']],
+    plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+    ],
   };
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,9 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from 'tamagui';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View flex={1} justifyContent='center' alignItems='center'>
+      <Button theme='blue'>Hello world</Button>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );

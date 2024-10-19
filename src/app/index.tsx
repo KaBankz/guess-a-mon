d3e67f85d6b0ae52c21118fba048126452f5e311
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-18 22:52:59 -0400

    Add more letters

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -14,16 +14,95 @@ export default function Index() {
         </YStack>
         <XStack gap='$2' alignItems='flex-end'>
           <YStack alignItems='center'>
-            <Text fontSize='$10'>S</Text>
+            <Text fontSize='$9'>S</Text>
             <Square
               height='$0.5'
-              width='$5'
+              width='$2'
               circular
               backgroundColor='$color'
             />
           </YStack>
-          <Square height='$0.5' width='$5' circular backgroundColor='$color' />
-          <Square height='$0.5' width='$5' circular backgroundColor='$color' />
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>E</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>A</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>K</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>I</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>N</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>G</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>G</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>G</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
+          <YStack alignItems='center'>
+            <Text fontSize='$9'>G</Text>
+            <Square
+              height='$0.5'
+              width='$2'
+              circular
+              backgroundColor='$color'
+            />
+          </YStack>
         </XStack>
         <Keyboard />
       </YStack>

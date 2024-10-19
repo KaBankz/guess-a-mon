3341d44e43514673130fa6aa07c46625e9afc936
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-19 13:13:03 -0400

    Add eslint, prettier and cspell

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+/* eslint-env node */
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  extends: [
+    'universe/native',
+    'universe/shared/typescript-analysis',
+    'plugin:tailwindcss/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    tailwindcss: {
+      callees: ['classnames', 'clsx', 'ctl', 'cva', 'cx', 'twMerge'],
+    },
+  },
+  rules: {
+    'import/order': 'off',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  ignorePatterns: ['/.expo', 'node_modules', '*.d.ts', 'ios', 'android'],
+};
+
+module.exports = config;

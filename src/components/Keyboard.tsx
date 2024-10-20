d7f8987bacd479df5ef884c6e2f2d20a771fdf0d
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-20 14:44:16 -0400

    Add row of special chars to keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -22,6 +22,7 @@ export const Keyboard = ({
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
     ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '←'],
+    ['-', "'", ' ', ':', '.'],
   ];
 
   return (
@@ -42,6 +43,7 @@ export const Keyboard = ({
                 letter === '←'
                   ? 'bg-red-500 active:bg-red-600 dark:bg-red-600 dark:active:bg-red-700'
                   : '',
+                letter === ' ' ? 'w-40' : '',
                 disabledKeys?.includes(letter) ? 'opacity-50' : ''
               )}>
               <Text
